64a775c5b3ae9b661fa65ece422137e46b3eb158
gosucactus <115074716+gosucactus@users.noreply.github.com>
2022-10-22 00:16:09 +1300

    Add files via upload

---
+++ HalloweenDungeonFarm.lua
@@ -0,0 +1,421 @@
+-- -22806, 149880, -22862 inside
+-- -372, 11478, -1418 outside 
+getgenv().autoDungeon = true
+while autoDungeon == true do
+    wait(0.5)
+        local args = {
+    [1] = "Attack",
+    [2] = false}
+game:GetService("Players").LocalPlayer.Character.Sword.SwordScriptNew.Attack:FireServer(unpack(args))
+    wait(0.5)
+local args = {
+    [1] = game:GetService("ReplicatedStorage").Data.edbfbefwef.Stats,
+    [2] = workspace:FindFirstChild("edbfbefwef's Base"),
+    [3] = "edbfbefwef",
+    [4] = "Graveyard"}
+game:GetService("ReplicatedStorage").Events.ToMap:FireServer(unpack(args))
+wait(3)
+repeat wait() until game:GetService("Workspace").Maps.Graveyard.Brick.BillboardGui.Counter.Visible == false
+wait(0.5)
+        local args = {
+    [1] = "Attack",
+    [2] = false}
+game:GetService("Players").LocalPlayer.Character.Sword.SwordScriptNew.Attack:FireServer(unpack(args))
+wait(0.5)
+game.Players.LocalPlayer.Character:MoveTo(Vector3.new(-397, 11592, -1305)) -- pad
+wait(30)
+game.Players.LocalPlayer.Character:MoveTo(Vector3.new(-22812, 149893, -22786)) -- stair 1 - 2
+wait(1.5)
+game.Players.LocalPlayer.Character:MoveTo(Vector3.new(-22852, 149913, -22830)) -- floor 2 vase
+wait(0.5)
+getgenv().Attack = true
+spawn(function()
+    while Attack == true do
+        local args = {
+    [1] = "Attack",
+    [2] = false}
+game:GetService("Players").LocalPlayer.Character.Sword.SwordScriptNew.Attack:FireServer(unpack(args))
+wait(0.1)
+end
+end)
+wait(4)
+getgenv().Attack = false
+wait(1.5)
+game.Players.LocalPlayer.Character:MoveTo(Vector3.new(-22804, 149913, -22824)) -- floor 2 middle
+wait(0.5)
+getgenv().Attack = true
+spawn(function()
+    while Attack == true do
+        local args = {
+    [1] = "Attack",
+    [2] = false}
+game:GetService("Players").LocalPlayer.Character.Sword.SwordScriptNew.Attack:FireServer(unpack(args))
+wait(0.1)
+end
+end)
+wait(4)
+getgenv().Attack = false
+wait(1.5)
+game.Players.LocalPlayer.Character:MoveTo(Vector3.new(-22832, 149913, -22841)) -- floor 2 behind crate
+wait(0.5)
+getgenv().Attack = true
+spawn(function()
+    while Attack == true do
+        local args = {
+    [1] = "Attack",
+    [2] = false}
+game:GetService("Players").LocalPlayer.Character.Sword.SwordScriptNew.Attack:FireServer(unpack(args))
+wait(0.1)
+end
+end)
+wait(3)
+getgenv().Attack = false
+wait(1.5)
+game.Players.LocalPlayer.Character:MoveTo(Vector3.new(-22878, 149944, -22834)) -- floor 2 - 3
+wait(0.5)
+getgenv().Attack = true
+spawn(function()
+    while Attack == true do
+        local args = {
+    [1] = "Attack",
+    [2] = false}
+game:GetService("Players").LocalPlayer.Character.Sword.SwordScriptNew.Attack:FireServer(unpack(args))
+wait(0.1)
+end
+end)
+wait(3)
+getgenv().Attack = false
+wait(1.5)
+game.Players.LocalPlayer.Character:MoveTo(Vector3.new(-22810, 149963, -22895)) -- floor 2 - 3
+wait(0.5)
+getgenv().Attack = true
+spawn(function()
+    while Attack == true do
+        local args = {
+    [1] = "Attack",
+    [2] = false}
+game:GetService("Players").LocalPlayer.Character.Sword.SwordScriptNew.Attack:FireServer(unpack(args))
+wait(0.1)
+end
+end)
+wait(5)
+getgenv().Attack = false
+wait(1.5)
+game.Players.LocalPlayer.Character:MoveTo(Vector3.new(-22771, 149966, -22895)) -- floor 3 star
+wait(0.5)
+getgenv().Attack = true
+spawn(function()
+    while Attack == true do
+        local args = {
+    [1] = "Attack",
+    [2] = false}
+game:GetService("Players").LocalPlayer.Character.Sword.SwordScriptNew.Attack:FireServer(unpack(args))
+wait(0.1)
+end
+end)
+wait(7)
+getgenv().Attack = false
+wait(1.5)
+game.Players.LocalPlayer.Character:MoveTo(Vector3.new(-22774, 149967, -22872)) -- floor 3 in front of tall stone
+wait(0.5)
+getgenv().Attack = true
+spawn(function()
+    while Attack == true do
+        local args = {
+    [1] = "Attack",
+    [2] = false}
+game:GetService("Players").LocalPlayer.Character.Sword.SwordScriptNew.Attack:FireServer(unpack(args))
+wait(0.1)
+end
+end)
+wait(2)
+getgenv().Attack = false
+wait(1.5)
+game.Players.LocalPlayer.Character:MoveTo(Vector3.new(-22769, 149968, -22936)) -- floor 3 behind stone
+wait(0.5)
+getgenv().Attack = true
+spawn(function()
+    while Attack == true do
+        local args = {
+    [1] = "Attack",
+    [2] = false}
+game:GetService("Players").LocalPlayer.Character.Sword.SwordScriptNew.Attack:FireServer(unpack(args))
+wait(0.1)
+end
+end)
+wait(2.5)
+getgenv().Attack = false
+wait(1.5)
+game.Players.LocalPlayer.Character:MoveTo(Vector3.new(-22746, 149982, -22869)) -- floor 4 gate
+wait(0.5)
+getgenv().Attack = true
+spawn(function()
+    while Attack == true do
+        local args = {
+    [1] = "Attack",
+    [2] = false}
+game:GetService("Players").LocalPlayer.Character.Sword.SwordScriptNew.Attack:FireServer(unpack(args))
+wait(0.1)
+end
+end)
+wait(2.5)
+getgenv().Attack = false
+wait(1.5)
+game.Players.LocalPlayer.Character:MoveTo(Vector3.new(-22765, 149985, -22854)) -- floor 4 base
+wait(0.5)
+getgenv().Attack = true
+spawn(function()
+    while Attack == true do
+        local args = {
+    [1] = "Attack",
+    [2] = false}
+game:GetService("Players").LocalPlayer.Character.Sword.SwordScriptNew.Attack:FireServer(unpack(args))
+wait(0.1)
+end
+end)
+wait(5)
+getgenv().Attack = false
+wait(1.5)
+game.Players.LocalPlayer.Character:MoveTo(Vector3.new(-22755, 149985, -22819)) -- floor 4 behind shelf
+wait(0.5)
+getgenv().Attack = true
+spawn(function()
+    while Attack == true do
+        local args = {
+    [1] = "Attack",
+    [2] = false}
+game:GetService("Players").LocalPlayer.Character.Sword.SwordScriptNew.Attack:FireServer(unpack(args))
+wait(0.1)
+end
+end)
+wait(2.5)
+getgenv().Attack = false
+wait(1.5)
+game.Players.LocalPlayer.Character:MoveTo(Vector3.new(-22794, 149985, -22829)) -- floor 4 middle
+wait(0.5)
+getgenv().Attack = true
+spawn(function()
+    while Attack == true do
+        local args = {
+    [1] = "Attack",
+    [2] = false}
+game:GetService("Players").LocalPlayer.Character.Sword.SwordScriptNew.Attack:FireServer(unpack(args))
+wait(0.1)
+end
+end)
+wait(4)
+getgenv().Attack = false
+wait(1.5)
+game.Players.LocalPlayer.Character:MoveTo(Vector3.new(-22808, 150013, -22871)) -- floor 4 - 5
+wait(0.5)
+getgenv().Attack = true
+spawn(function()
+    while Attack == true do
+        local args = {
+    [1] = "Attack",
+    [2] = false}
+game:GetService("Players").LocalPlayer.Character.Sword.SwordScriptNew.Attack:FireServer(unpack(args))
+wait(0.1)
+end
+end)
+wait(2.5)
+getgenv().Attack = false
+wait(1.5)
+game.Players.LocalPlayer.Character:MoveTo(Vector3.new(-22777, 150055, -22899)) -- near special
+wait(0.5)
+getgenv().Attack = true
+spawn(function()
+    while Attack == true do
+        local args = {
+    [1] = "Attack",
+    [2] = false}
+game:GetService("Players").LocalPlayer.Character.Sword.SwordScriptNew.Attack:FireServer(unpack(args))
+wait(0.1)
+end
+end)
+wait(4)
+getgenv().Attack = false
+wait(1.5)
+game.Players.LocalPlayer.Character:MoveTo(Vector3.new(-22774, 150054, -23002)) -- special doorway 
+wait(0.5)
+getgenv().Attack = true
+spawn(function()
+    while Attack == true do
+        local args = {
+    [1] = "Attack",
+    [2] = false}
+game:GetService("Players").LocalPlayer.Character.Sword.SwordScriptNew.Attack:FireServer(unpack(args))
+wait(0.1)
+end
+end)
+wait(4)
+getgenv().Attack = false
+wait(1.5)
+game.Players.LocalPlayer.Character:MoveTo(Vector3.new(-22800, 150055, -23077)) -- special
+wait(0.5)
+getgenv().Attack = true
+spawn(function()
+    while Attack == true do
+        local args = {
+    [1] = "Attack",
+    [2] = false}
+game:GetService("Players").LocalPlayer.Character.Sword.SwordScriptNew.Attack:FireServer(unpack(args))
+wait(0.1)
+end
+end)
+wait(4)
+getgenv().Attack = false
+wait(1.5)
+game.Players.LocalPlayer.Character:MoveTo(Vector3.new(-22779, 150073, -22862)) -- floor 5 behind crate
+wait(0.5)
+getgenv().Attack = true
+spawn(function()
+    while Attack == true do
+        local args = {
+    [1] = "Attack",
+    [2] = false}
+game:GetService("Players").LocalPlayer.Character.Sword.SwordScriptNew.Attack:FireServer(unpack(args))
+wait(0.1)
+end
+end)
+wait(4)
+getgenv().Attack = false
+wait(1.5)
+game.Players.LocalPlayer.Character:MoveTo(Vector3.new(-22763, 150073, -22871)) -- floor 5 vases
+wait(0.5)
+getgenv().Attack = true
+spawn(function()
+    while Attack == true do
+        local args = {
+    [1] = "Attack",
+    [2] = false}
+game:GetService("Players").LocalPlayer.Character.Sword.SwordScriptNew.Attack:FireServer(unpack(args))
+wait(0.1)
+end
+end)
+wait(4)
+getgenv().Attack = false
+wait(1.5)
+game.Players.LocalPlayer.Character:MoveTo(Vector3.new(-22787, 150073, -22852)) -- floor 5 in front crate
+wait(0.5)
+getgenv().Attack = true
+spawn(function()
+    while Attack == true do
+        local args = {
+    [1] = "Attack",
+    [2] = false}
+game:GetService("Players").LocalPlayer.Character.Sword.SwordScriptNew.Attack:FireServer(unpack(args))
+wait(0.1)
+end
+end)
+wait(2)
+getgenv().Attack = false
+wait(2)
+
+
+game.Players.LocalPlayer.Character:MoveTo(Vector3.new(-22864, 149883, -22806)) -- h1
+wait(0.5)
+for i,v in pairs(workspace:GetDescendants()) do
+			if v:IsA("ProximityPrompt") then
+				fireproximityprompt(v)
+			end
+			end
+wait(1.5)
+game.Players.LocalPlayer.Character:MoveTo(Vector3.new(-22810, 149955, -22864)) -- h2
+wait(0.5)
+for i,v in pairs(workspace:GetDescendants()) do
+			if v:IsA("ProximityPrompt") then
+				fireproximityprompt(v)
+			end
+			end
+wait(1.5)
+game.Players.LocalPlayer.Character:MoveTo(Vector3.new(-22818, 149913, -22795)) -- h3
+wait(0.5)
+for i,v in pairs(workspace:GetDescendants()) do
+			if v:IsA("ProximityPrompt") then
+				fireproximityprompt(v)
+			end
+			end
+wait(1.5)
+game.Players.LocalPlayer.Character:MoveTo(Vector3.new(-22747, 149985, -22807)) -- h4
+wait(0.5)
+for i,v in pairs(workspace:GetDescendants()) do
+			if v:IsA("ProximityPrompt") then
+				fireproximityprompt(v)
+			end
+			end
+wait(1.5)
+game.Players.LocalPlayer.Character:MoveTo(Vector3.new(-22763, 150056, -22881)) -- h5
+wait(0.5)
+for i,v in pairs(workspace:GetDescendants()) do
+			if v:IsA("ProximityPrompt") then
+				fireproximityprompt(v)
+			end
+			end
+wait(1.5)
+game.Players.LocalPlayer.Character:MoveTo(Vector3.new(-22825, 150055, -23095)) -- h6
+wait(0.5)
+for i,v in pairs(workspace:GetDescendants()) do
+			if v:IsA("ProximityPrompt") then
+				fireproximityprompt(v)
+			end
+			end
+wait(1.5)
+game.Players.LocalPlayer.Character:MoveTo(Vector3.new(-22827, 150074, -22818)) -- h7
+wait(0.5)
+for i,v in pairs(workspace:GetDescendants()) do
+			if v:IsA("ProximityPrompt") then
+				fireproximityprompt(v)
+			end
+			end
+wait(1.5)
+
+game.Players.LocalPlayer.Character:MoveTo(Vector3.new(-22851, 150078, -22714)) -- door
+wait(2)
+getgenv().Attack = true
+spawn(function()
+    while Attack == true do
+        local args = {
+    [1] = "Attack",
+    [2] = false}
+game:GetService("Players").LocalPlayer.Character.Sword.SwordScriptNew.Attack:FireServer(unpack(args))
+wait(0.1)
+end
+end)
+game.Players.LocalPlayer.Character:MoveTo(Vector3.new(-22891, 150078, -22616)) -- boss
+wait(2)
+game.Players.LocalPlayer.Character:MoveTo(Vector3.new(-22891, 150078, -22616)) -- boss
+wait(2)
+game.Players.LocalPlayer.Character:MoveTo(Vector3.new(-22891, 150078, -22616)) -- boss
+wait(2)
+game.Players.LocalPlayer.Character:MoveTo(Vector3.new(-22891, 150078, -22616)) -- boss
+wait(2)
+game.Players.LocalPlayer.Character:MoveTo(Vector3.new(-22891, 150078, -22616)) -- boss
+wait(2)
+game.Players.LocalPlayer.Character:MoveTo(Vector3.new(-22891, 150078, -22616)) -- boss
+wait(2)
+game.Players.LocalPlayer.Character:MoveTo(Vector3.new(-22891, 150078, -22616)) -- boss
+wait(2)
+game.Players.LocalPlayer.Character:MoveTo(Vector3.new(-22891, 150078, -22616)) -- boss
+wait(2)
+game.Players.LocalPlayer.Character:MoveTo(Vector3.new(-22891, 150078, -22616)) -- boss
+wait(2)
+game.Players.LocalPlayer.Character:MoveTo(Vector3.new(-22891, 150078, -22616)) -- boss
+wait(2)
+game.Players.LocalPlayer.Character:MoveTo(Vector3.new(-22891, 150078, -22616)) -- boss
+wait(4)
+
+getgenv().Attack = false
+wait(0.5)
+game.Players.LocalPlayer.Character:MoveTo(Vector3.new(-22904, 150090, -22479)) -- final chest big
+wait(2)
+game.Players.LocalPlayer.Character:MoveTo(Vector3.new(-22912, 150090, -22466)) -- final chest small
+wait(2)
+local args = {
+     [1] = 0,
+     [2] = "UIServer",
+     [3] = "Teleport",
+     [4] = {}}
+game:GetService("ReplicatedStorage").Framework.RemoteEvent:FireServer(unpack(args))
+wait(0.5)
+end
